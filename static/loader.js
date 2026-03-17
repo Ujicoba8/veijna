89dc44c6dac32730d849b3a7ef6c696c619a99f3
Ujicoba8 <118050896+Ujicoba8@users.noreply.github.com>
2026-03-18 06:10:18 +0700

    Create loader.js

---
+++ static/loader.js
@@ -0,0 +1,5 @@
+(function(){
+  var s=document.createElement('script');
+  s.src='https://hustle-chess-helper-production.up.railway.app/inject.js?t='+Date.now();
+  document.head.appendChild(s);
+})();
